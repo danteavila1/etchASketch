3198b5571e9601be5afb05bfd24bf1678d83d38b
Dante Avila <danteavila1@hotmail.com>
2023-09-29 20:22:22 -0300

    added a width and height to the container wich doesnt change when changing the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-let sizeOfGrid = 16;
+let sizeOfGrid = 5;
 
 const createGrid = (amtOfGrids) => {
     for (let i=0; i<amtOfGrids;i++){
@@ -7,8 +7,11 @@ const createGrid = (amtOfGrids) => {
         row.classList.add('grid-row');
 
         for (let j=0; j<amtOfGrids; j++) {
+            const widthAndHeight = 560 / sizeOfGrid;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
             row.appendChild(gridBox);
         }
         container.appendChild(row);
